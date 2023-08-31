a246c666f0d5af8da160125ac900f1035ef51722
Ty <Ty@Tys-Macbook.local>
2023-08-31 16:31:12 -0400

    github links added

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -16,7 +16,10 @@ function ProjectsPage() {
                     <br></br>
                     <div className="project1_text3">My favorite parts of this app were figuring out how to make the pieces pinnable so they cannot move if they are protecting the king, checkmate, and castling functionality.</div>
                     <br></br>
-                    <a className="live_link1" href="https://tykirkland.github.io/Project-1/" target="_blank">Live Link</a>
+                    <div className="links">
+                        <a className="live_link1" href="https://tykirkland.github.io/Project-1/" target="_blank">Live Link</a>
+                        <a className="github_link1" href="https://github.com/TyKirkland/Project-1.git" target="_blank">GitHub</a>
+                    </div>
                 </div>
             </div>
             <hr></hr>
@@ -29,7 +32,10 @@ function ProjectsPage() {
                     <br></br>
                     <div className="project2_text3">It was made using Javascript, CSS, HTML, EJS, Node, Express and MongoDB.</div>
                     <br></br>
-                    <a className="live_link2" href="https://ezemployees.onrender.com/" target="_blank">Live Link</a>
+                    <div className="links">
+                        <a className="live_link2" href="https://ezemployees.onrender.com/" target="_blank">Live Link</a>
+                        <a className="github_link2" href="https://github.com/TyKirkland/EZEmployees" target="_blank">GitHub</a>
+                    </div>
                 </div>
                 <a className="project2_link" href="https://ezemployees.onrender.com/" target="_blank">
                     <img className="project2_photo" src={process.env.PUBLIC_URL + '/EZEmployeesScreenShot.png'} alt="EZEmployees Photo"></img>
@@ -46,7 +52,10 @@ function ProjectsPage() {
                     <br></br>
                     <div className="project3_text2">I created this full-stack application using React, Express, MongoDB, and Node.js.</div>
                     <br></br>
-                    <a className="live_link3" href="https://teampro3-front.onrender.com/" target="_blank">Live Link</a>
+                    <div className="links">
+                        <a className="live_link3" href="https://teampro3-front.onrender.com/" target="_blank">Live Link</a>
+                        <a className="github_link3" href="https://github.com/jotavo13/TEAM_PRO_3" target="_blank">GitHub</a>
+                    </div>
                 </div>
             </div>
             <hr></hr>
@@ -59,7 +68,10 @@ function ProjectsPage() {
                     <br></br>
                     <div className="project4_text3">I created this full-stack application using Django, Python, HTML, CSS and SQL databases.</div>
                     <br></br>
-                    <a className="live_link4" href="https://battle-arena.onrender.com/" target="_blank">Live Link</a>
+                    <div className="links">
+                        <a className="live_link4" href="https://battle-arena.onrender.com/" target="_blank">Live Link</a>
+                        <a className="github_link4" href="https://github.com/TyKirkland/Capstone_Project" target="_blank">GitHub</a>
+                    </div>
                 </div>
                 <a className="project4_link" href="https://battle-arena.onrender.com/" target="_blank">
                     <img className="project4_photo1" src={process.env.PUBLIC_URL + '/BattleArenaScreenShot1.png'} alt="BattleArena Photo"></img>
